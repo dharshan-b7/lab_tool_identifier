860f3c9fcb0e1c1402b7646041cb8ab144ad45d9
bdharshan1729 <bdharshan1729@gmail.com>
2025-07-11 12:38:03 +0000

    Initial commit

--- main.py
+++ main.py
@@ -76,4 +76,5 @@ def index():
     return render_template("index.html", response_text=response_text, image_url=image_url, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+
